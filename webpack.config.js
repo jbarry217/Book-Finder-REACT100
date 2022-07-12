57dbaa7d3928e0cb5d34c058363960bdf8c304d1
Jen <jenniferlynnebarry@gmail.com>
2022-07-12 14:47:18 -0400

    still not loading api in heroku

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,18 @@
 const path = require('path');
 const webpack = require('webpack');
-// const dotenv = require('dotenv');
+const dotenv = require('dotenv');
 
-// const env = dotenv.config().parsed
-// const envKeys = Object.keys(env).reduce((prev, next) => {
-//   prev[`process.env.${next}`] = JSON.stringify(env[next])
-//   return prev
-// }, {})
+const env = dotenv.config().parsed
+const envKeys = Object.keys(env).reduce((prev, next) => {
+  prev[`process.env.${next}`] = JSON.stringify(env[next])
+  return prev
+}, {})
 
 module.exports = {
 
-  // plugins: [
-  //   new webpack.DefinePlugin(envKeys)
-  // ],
+  plugins: [
+    new webpack.DefinePlugin(envKeys)
+  ],
 
   context: path.join(__dirname, '/src'),
 
