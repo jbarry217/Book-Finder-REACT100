0d36f81dc7e751bca7c7cba032f3a0c7538c2b37
Jen <jenniferlynnebarry@gmail.com>
2022-07-12 15:04:57 -0400

    heroku updates

--- server/server.js
+++ server/server.js
@@ -9,25 +9,25 @@ const app = express();
 app.use(express.static('dist'));
 app.use(express.static('public'));
 
-app.get('/api', (req, res) => {
+// app.get('/api', (req, res) => {
 
-   if(props.searchObj.select == "author"){
-        var URL = `https://www.googleapis.com/books/v1/volumes?q=+inauthor:&key={api_key}`
-    } else if (props.searchObj.select == "title"){
-        var URL = `https://www.googleapis.com/books/v1/volumes?q=+intitle:&key={api_key}`
-    }
-    axios.get(URL)
-        .then((bookObj) => {
-            bookList.push(bookObj.data);
-            res.send(bookObj.data)
-        })
-        .then(bookList => this.setState({ bookList }))
-        .catch((error) => {
-            console.error(error);
-            res.send('An error occured.');
-        })
+//    if(props.searchObj.select == "author"){
+//         var URL = `https://www.googleapis.com/books/v1/volumes?q=+inauthor:&key={api_key}`
+//     } else if (props.searchObj.select == "title"){
+//         var URL = `https://www.googleapis.com/books/v1/volumes?q=+intitle:&key={api_key}`
+//     }
+//     axios.get(URL)
+//         .then((bookObj) => {
+//             bookList.push(bookObj.data);
+//             res.send(bookObj.data)
+//         })
+//         .then(bookList => this.setState({ bookList }))
+//         .catch((error) => {
+//             console.error(error);
+//             res.send('An error occured.');
+//         })
     
-});
+// });
 
 
 module.exports = app;
